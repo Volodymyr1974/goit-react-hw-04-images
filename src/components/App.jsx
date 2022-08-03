2513ae30dca8cb8135e0ec95933976043f54f296
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-08-03 15:14:57 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,36 @@
 import Searchbar from './Searchbar/Searchbar';
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ImageGallery from './ImageGallery/ImageGallery';
 import style from './App.module.css';
 import Notiflix from 'notiflix';
 
-export default class App extends Component {
-  state = {
-    qwery: '',
-    page: 1,
-  }
-  searchTextSubmit = (qwery) => {
-    if (qwery === this.state.qwery) {
+export default function App() {
+  const [qwery, setQwery] = useState('');
+  const [page, setPage] = useState(1);
+
+  const searchTextSubmit = (textqwery) => {
+    if (textqwery === qwery) {
       return Notiflix.Notify.failure('Вибачте, такий запит вже оброблено. Введіть інший запит для пошуку, або натисніть  "Load More');
     }
-    this.setState(
-      {
-        qwery,
-        page: 1
-      });
-  };
-  loadMoreBtn = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
+    setQwery(textqwery);
+    setPage(1);
 
-    }));
+  };
+  const loadMoreBtn = () => {
+
+    setPage(state => state + 1);
   };
 
-  render() {
-    const { qwery, page } = this.state;
-    return (
-      <div className={style.App} >
-        <Searchbar onSubmit={this.searchTextSubmit} />
-        <ImageGallery
-          searchQwery={qwery}
-          pageNumber={page}
-          onLoadMoreBtn={this.loadMoreBtn}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={style.App} >
+      <Searchbar onSubmit={searchTextSubmit} />
+      <ImageGallery
+        searchQwery={qwery}
+        pageNumber={page}
+        onLoadMoreBtn={loadMoreBtn}
+      />
+    </div>
+  );
+
 
 };
